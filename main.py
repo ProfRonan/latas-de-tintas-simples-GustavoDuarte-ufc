f766e9bd859320144787d38eaa38e140500f9c59
GustavoDuarte-ufc <gustavoduarte2804@gmail.com>
2023-03-25 21:01:23 -0300

    Adicionado

--- main.py
+++ main.py
@@ -3,10 +3,21 @@ metros_quadrados = input("Qual a área em m²?\n")
 metros_quadrados = float(metros_quadrados)
 
 # Coloque o código para resolver o problema nessa parte do programa
+qtd_de_latas = 0
+valor_total = 0
+cobertura = metros_quadrados / 3
+if cobertura > 18:
+    qtd_de_latas = int(cobertura / 18) + 1
+    valor_total = qtd_de_latas * 80
+    print(f"Serão necessárias {qtd_de_latas} latas totalizando R$ {valor_total}")
+elif metros_quadrados == 0:
+    qtd_de_latas = 0
+    valor_total = 0
+    print(f"Serão necessárias {qtd_de_latas} latas totalizando R$ {valor_total}")
+else:
+    qtd_de_latas = 1
+    valor_total = qtd_de_latas * 80
+    print(f"Serão necessárias {qtd_de_latas} latas totalizando R$ {valor_total}")
 
 # As duas variáveis qtd_de_latas e valor_total devem guardar a resposta do problema
 # Troque os zeros pelos valores corretos.
-qtd_de_latas = 0
-valor_total = 0
-
-print(f"Serão necessárias {qtd_de_latas} latas totalizando R$ {valor_total}")
